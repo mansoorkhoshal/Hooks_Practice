e048ed89a5081f0e656ec61020c44b55cd798b9f
Mansoor Ahmad <mansoorahmad.dev44@gmail.com>
2025-10-18 16:55:25 +0500

    Controlled Input State

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+
+  const [inputValue, setInputValue] = useState("");
 
   return (
-    <div>
-      <p>You Clicked me {count}</p>
+    // <div>
+    //   <p>You Clicked {count} Times</p>
 
-      <button onClick={() => setCount(count + 1)} className="outline rounded px-4 bg-green-400">Click Me</button>
+    //   <button onClick={() => setCount(count + 1)} className="outline rounded px-4 bg-green-400">Click Me</button>
+
+    // </div>
+
+    <div className="p-5">
+      <input
+        className="bg-green-300 py-2 pl-1 mb-3 outline rounded"
+        type="text" placeholder="Type Something here..." value={inputValue} onChange={(e) => setInputValue(e.target.value)}
+      />
+      <p>
+        You Typed : <strong>{inputValue}</strong>
+      </p>
     </div>
   );
 };
